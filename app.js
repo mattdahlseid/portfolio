1c3cc56e36740e530b02a6a86ef363a0f47a0d25
Matt-Dahlseid <matt.dahlseid@gmail.com>
2018-11-11 00:15:42 -0800

    Updates to contact/work sections + added photos

--- app.js
+++ app.js
@@ -3,17 +3,82 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
-    }, 700);
+    }, 500);
 });
 
 (function() {
-    let skillsEl = $('div.skills-tools-box'),
+    let skillsEl = $(".skills-tools-box"),
         skillsElOffset = skillsEl.offset().top/2,
         documentEl = $(document);
 
         documentEl.on('scroll', function() {
-            if (documentEl.scrollTop() > skillsElOffset && skillsEl.hasClass('hidden')) {
-                skillsEl.removeClass('hidden');
+            if (documentEl.scrollTop() > skillsElOffset && skillsEl.hasClass("hidden")) {
+                skillsEl.removeClass("hidden");
             }
         });
 })();
+
+(function() {
+    let skillsPeek = $(".about__skills-peek"),
+        skillsPeekOffset = skillsPeek.offset().top/100,
+        documentEl = $(document);
+
+        documentEl.on('scroll', function() {
+            if (documentEl.scrollTop() > skillsPeekOffset) {
+                skillsPeek.fadeOut();
+            } else {
+                skillsPeek.fadeIn();
+            }
+        });
+})();
+
+
+// grow project container on click
+let contentBox = $(".work__projects__container");
+let imageBox = $(".work__projects__box");
+//let title = $(".work__projects__box__title");
+imageBox.on("click", function() {
+    if ($(this).parent().hasClass("work__projects__container__shrink")) {
+        $(this).parent().toggleClass("work__projects__container__shrink work__projects__container__grow");
+        $(this).find(".work__projects__box__title").toggleClass("work__projects__box__title__back work__projects__box__title__noBack");
+        //title.toggleClass("work__projects__box__title__back work__projects__box__title__noBack")
+    } else if ($(this).parent().hasClass("work__projects__container__grow")) {
+        $(this).parent().toggleClass("work__projects__container__grow work__projects__container__shrink");
+        $(this).find(".work__projects__box__title").toggleClass("work__projects__box__title__noBack work__projects__box__title__back");
+    } else {
+        $(this).parent().addClass("work__projects__container__grow");
+       $(this).find(".work__projects__box__title").addClass("work__projects__box__title__back");
+    }
+})
+
+
+/*$(document).scroll(function() {
+    var y = $(document).scrollTop(), //get page y value 
+        header = $(".section-work__bar__head"),
+        vh = $('.header').height() + $('#about').height(),
+        heightWithWork = vh + $('#work');
+    if (y >= vh )  {
+        header.css({"position" : "fixed", "top" : "0", "left" : "0", "font-size" : "4rem", "text-align" : "center"});
+        $("body").append(header);
+    } else {
+        $("body").remove(header);
+        header.css("position", "relative");
+    }
+})*/
+
+// handle visibility of menu icon
+$(document).scroll(function() {
+    var y = $(document).scrollTop(), //get page y value 
+        menu = $(".menu"),
+        headerHeight = $('.header').height();
+    if ( y >= (headerHeight - 50))  {
+        menu.fadeIn();
+    } else {
+        $('input[type=checkbox]').prop('checked',false);
+        menu.fadeOut();
+    }
+})
+
+$('.menu ul li a').on("click", function (e) {
+    $('input[type=checkbox]').prop('checked',false);
+});
